944293be2f5def7d8fab75b562c7cd85ca627c5e
vikas <dvikas793@gmail.com>
2024-04-09 16:22:54 +0530

    removed accounts

--- testaccount/terraform/s3.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "testaccount13111993" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
-
--- testaccount/terraform/ssm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "foo" {
-  name	= "/accId"
-  type  = "string"
-  value = "${data.aws_caller_identity.current.account_id}"
-}
