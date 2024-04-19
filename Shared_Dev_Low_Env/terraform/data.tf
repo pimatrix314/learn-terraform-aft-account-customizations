4ef4623900b2e3d87b7935bdd318e3f4225a7eb7
vikas <dvikas793@gmail.com>
2024-04-20 00:10:23 +0530

    add new account

--- Shared_Dev_Low_Env/terraform/data.tf
+++
@@ -1,9 +0,0 @@
-
-# AFT Management account id
-data "aws_caller_identity" "aft_management_account" {
-  provider = aws.aft_management_account
-}
- 
-data "aws_caller_identity" "current" {}
- 
-data "aws_region" "current" {}
